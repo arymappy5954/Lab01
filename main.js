74284a934ac56c45afddf67508f277c5c51c3688
aranya <aranya.9545@gmail.com>
2022-06-30 21:39:48 +0700

    6.5-6.6

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ const app = Vue.createApp({
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2234, color: 'blue'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2234, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
 
             sizes: ['S','M','L'],
             onSale: true,
+            description: 'Leather',
             link: 'https://www.camt.cmu.ac.th/',
 
             cart: 0
@@ -21,6 +22,9 @@ const app = Vue.createApp({
     methods: {
         addToCart() {
             this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
