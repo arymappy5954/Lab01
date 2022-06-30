76fa3cb38a70529f7d5e7f813f49b84e6cc8bab7
aranya <aranya.9545@gmail.com>
2022-06-30 21:31:14 +0700

    4.9,5.3-5.4,6.1-6.4

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ const app = Vue.createApp({
             inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2234, color: 'blue'}
+            ],
 
-            link: 'https://www.camt.cmu.ac.th/'
+            sizes: ['S','M','L'],
+            onSale: true,
+            link: 'https://www.camt.cmu.ac.th/',
+
+            cart: 0
+        } 
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
         }
-        
     }
 })
