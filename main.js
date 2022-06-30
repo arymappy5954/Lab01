4f30fe9a00f7c9889418c9746957126b8fc84d71
aranya <aranya.9545@gmail.com>
2022-06-30 21:12:31 +0700

    4.7-4.8,5.1-5.2

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inStock: true,
             inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
 
             link: 'https://www.camt.cmu.ac.th/'
         }
